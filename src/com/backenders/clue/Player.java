c9c12be6eceb6ff9d9d245b0c3a714f53e8727cd
Gina Villegas <ginayvillegas@gmail.com>
2020-11-09 14:20:55 -0800

    updates to Player and Solution

--- src/com/backenders/clue/Player.java
+++ src/com/backenders/clue/Player.java
@@ -1,11 +1,11 @@
 package com.backenders.clue;
 
 public class Player {
-    Player hp = new Player;
+    private Enum currentRoom;
     private String name;
 
     // Constructor
-    public Player(String name, PlayerInput scan) { // or Scanner?
+    public Player(String name, Enum currentRoom) { // or Scanner?
         this.name = name;
     }
 
@@ -14,69 +14,27 @@ public class Player {
     // Inner class
     private class Guess {
         // Fields
-        Weapon weapon;
+        Weapon weaponGuess;
         RolePlayer rolePlayerGuess;
 
         // Constructor
         public Guess(Weapon weapon, RolePlayer rolePlayer) {
-            this.weapon = weapon;
-            this.rolePlayer = rolePlayer;
+            this.weaponGuess = weapon;
+            this.rolePlayerGuess = rolePlayer;
         }
 
         // Business Methods
 
-        Enum<RolePlayer> getGuess() {
-            Enum rolePlayerGuess;
-            int rpGuess;
 
-            System.out.println("Are you ready to guess? Enter the corresponding digit of the suspected murderer"
-                    + "(No spaces, please. Only a digit.)");
-            rpGuess = PlayerInput.scan.nextInt();
-            if ( 0 < rolePlayerGuess && rpGuess < 10){
-                switch (rpGuess) {
-                    case 1: setRolePlayerGuess(RolePlayer.GURU_JAY);
-                            break;
-                    case 2: setRolePlayerGuess(RolePlayer.MRS_PEACOCK);
-                            break;
-                    case 3: setRolePlayerGuess(RolePlayer.MRS_ORCHID);
-                            break;
-                    case 4: setRolePlayerGuess(RolePlayer.COLONEL_MUSTARD);
-                            break;
-                    case 5: setRolePlayerGuess(RolePlayer.MR_GREEN);
-                            break;
-                    case 6: setRolePlayerGuess(RolePlayer.MISS_SCARLET);
-                            break;
-                    case 7: setRolePlayerGuess(RolePlayer.CAPTAIN_CANDACE);
-                            break;
-                    case 8: setRolePlayerGuess(RolePlayer.PROFESSOR_EDGAR);
-                            break;
-                    case 9: setRolePlayerGuess(RolePlayer.GEMOLOGIST_GINA);
-                            break;
-                }
-            }
-            return rolePlayerGuess;
-        }
 
-        private boolean isInputValid(String guess) {
-            boolean isValid = false;
-            String inputPattern = "[1-9{1}]";   //"[1-9{1}],{1}[1-9{1}]";
-            isValid = guess.matches(inputPattern);
-            if (!isValid) {
-                System.out.println("Please enter a digit comma digit with no spaces:");
-            }
-            else {
-                isValid = true;
-            }
-            return isValid;
-        }
 
         // Accessor Methods
         public Weapon getWeaponGuess() {
-            return weapon;
+            return weaponGuess;
         }
 
         public void setWeaponGuess(Weapon weapon) {
-            this.weapon = weapon;
+            this.weaponGuess = weapon;
         }
 
         public RolePlayer getRolePlayerGuess() {
@@ -93,22 +51,21 @@ public class Player {
         return name;
     }
 
-    // I THINK I created this thing called dependency injection with the scanner
-    // so I'm supposed to pass the scan into the instantiation of Player in main
-    // something like this? (But I don't really know?)
-    // public static void main(String[] args) {
-    //    PlayerInput scan = new PlayerInput(System.in);
-    //    Player player = new Player(scan);
-    //    Player.getName();
     public void setName(String name) {
-        //System.out.println("Your player name can be anything except blank. " +
-                "Please enter a player name:");
-        //name = PlayerInput.scan.nextLine();
+
         if (name.isBlank()){
             System.out.println("No blanks allowed. Please enter a player name:");
         }
         else{
             this.name = name;
         }
+    }
+
+    public Enum getCurrentRoom() {
+        return currentRoom;
+    }
+
+    public void setCurrentRoom(Enum currentRoom) {
+        this.currentRoom = currentRoom;
     }
 }
