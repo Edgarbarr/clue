eff1185eac1832da12eca5f7bc8710afcb0cf145
Candace Lewis <candace@Mac-mini.local>
2020-11-09 13:17:25 -0500

    Candace rooms

---
+++ src/com/backenders/clue/RoomType.java
@@ -0,0 +1,25 @@
+package com.backenders.clue;
+
+public enum RoomType {
+    public enum RoomType {
+        KITCHEN("A large chef's kitchen, filled with knives, heavy pots, and delicious smells."),
+        BALLROOM("Sliding and gliding on hardwood waxed floors, a place fit for Fred and Ginger"),
+        BILLIARD_ROOM("The sound of the cue ball smacking the eight-ball. The smell of chalk. And you see two beautiful green felt tables are in the center of the room"),
+        LIBRARY("shh, books from floor to ceiling, easy chairs and read light abound"),
+        BEDROOM("King size bed, mirrors, a fluffy rug, and an alarm clock."),
+        HALL("Connects to all the rooms. There is an umbrella stand by the main entry."),
+        LOUNGE("Comfy chairs, people lounging and chatting"),
+        DININGROOM("A large table filled with dirty dishes and remanents of the meal. Is a butter knife missing?"),
+        CELLAR("Well stocked with aging and drink now wines, Scotts Whisky, Carribean Rum, and top shelf Tequila"),
+        SHOWER_ROOM("All good murders happen in the shower. Would you like to linger for one?");
+
+        private final String name;
+
+        private RoomType(String name) {
+            this.name = name;
+        }
+
+        public String toString() {
+            return this.name;
+
+        }
